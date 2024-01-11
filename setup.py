bbbeafe83856017b8958cb75df358797b1a54478
dad2jrn <dad2jrn@gmail.com>
2024-01-11 14:44:13 -0500

    Title: `chore(setup.py): update package details and bump version to 0.3`
    Body:
    Updated the `serpentscribe` package details in setup.py to provide more descriptive information about its functionality. The version has been bumped from 0.2 to 0.3 reflecting these changes and potential other improvements not specified in the diff. Also standardized string quotes to double quotes for consistency across the file.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='serpentscribe',
-    version='0.2',
+    name="serpentscribe",
+    version="0.3",
     packages=find_packages(),
-    license='MIT',
-    description='A fast and simple logging decorator for Python functions.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Ron Meck',
-    author_email='dad2jrn@gmail.com',
-    url='https://github.com/dad2jrn/serpentscribe',
+    license="MIT",
+    description="A fast, simple, and yet powerful Python logging library designed to make function call logging effortless. No longer do you have to instantiate logging multiple times within a function or class. This module provides a simple decorator to automatically log the details of function calls, including arguments and return values to stdout and optionally to a JSON file specified.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ron Meck",
+    author_email="dad2jrn@gmail.com",
+    url="https://github.com/dad2jrn/serpentscribe",
     install_requires=[
         # Dependencies, if any
     ],
